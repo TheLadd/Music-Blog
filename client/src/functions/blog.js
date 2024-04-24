b9714861273c0c54194a4308233c67bda733a7e3
TheLadd <owenribera@gmail.com>
2024-04-24 14:57:24 -0400

    Trying to change env vars to be normal instead of configured for React, in hopes that it allows env vars set from Netlify UI to be available at runtime

--- client/src/functions/blog.js
+++ client/src/functions/blog.js
@@ -1,8 +1,8 @@
 // A function that makes an HTTP request to BACKEND_URL
 export const getPosts = async () => {
     try {
-        console.log(`backend_url: ${process.env.REACT_APP_BACKEND_URL}`)
-        const response = await fetch(process.env.REACT_APP_BACKEND_URL + '/blog/get', {
+        // const response = await fetch(process.env.REACT_APP_BACKEND_URL + 'blog/get', {
+        const response = await fetch(process.env.BACKEND_URL + '/blog/get', {
             method: 'GET',
             headers: {
                 Accept: "application/json",
@@ -12,6 +12,7 @@ export const getPosts = async () => {
         return await response.json()
     } 
     catch (err) {
-        console.log(`Error occured fetching posts from heehee haha ${process.env.REACT_APP_BACKEND_URL}\n`, err)
+        // console.log(`Error occured fetching posts from ${process.env.REACT_APP_BACKEND_URL}\n`, err)
+        console.log(`Error occured fetching posts from ${process.env.BACKEND_URL}\n`, err)
     }
 }
